6d047938236a4a15a34522070fe2342df2de916e
Alexandr Varabyou <vorobev.profi@gmail.com>
2016-09-10 19:02:13 +0300

    queue constructor done

--- src/queue.js
+++ src/queue.js
@@ -2,7 +2,12 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		if(!maxSize){
+			this.maxSize = 30;
+		}else{
+			this.maxSize = maxSize;
+		}
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
