ac593d4d89f31561c632ed8cea7bbb4574411193
Alexandr Varabyou <vorobev.profi@gmail.com>
2016-10-08 17:45:30 +0300

    add swapWithParent method

--- src/node.js
+++ src/node.js
@@ -39,7 +39,60 @@ class Node {
 	}
 
 	swapWithParent() {
-		
+		if(this.parent == null){
+			return;
+		}
+
+		// Copy an environment of node.
+		var parentParent = this.parent.parent;
+		var parent = this.parent;
+		var parentLeft = this.parent.left;
+		var parentRight = this.parent.right;
+		var thisCopy = this;
+		var thisLeft = this.left;
+		var thisRight = this.right;
+
+		// Change parent property in swappedUp node and swappedDown node.
+		parent.parent = thisCopy;
+		thisCopy.parent = parentParent;
+
+		// If parent of parent exist, set swappedUp node to correct place of parent.parent node.
+		if(parentParent != null){
+			if(parentParent.left == parent){
+				parentParent.left = thisCopy;
+			}else if(parentParent.right == parent){
+				parentParent.right = thisCopy;
+			}
+		}
+
+		// Set swappedDown node to correct left or right place of swappedUp node
+		if(parentLeft == thisCopy){
+			thisCopy.left = parent;
+			thisCopy.right = parentRight;
+			// If parent.right node exist, set feedback with swappedUp node
+			if(parentRight != null){
+				parentRight.parent = thisCopy;
+			}
+		}else if(parentRight == thisCopy){
+			thisCopy.right = parent;
+			thisCopy.left = parentLeft;
+			// If parent.left exist, set feedback with swappedUp node
+			if(parentLeft != null){
+				parentLeft.parent = thisCopy;
+			}
+		}
+
+		// Update left and right properties of swappedDown node
+		parent.left = thisLeft;
+		parent.right = thisRight;
+
+		// If children of swappedUp node are exist, set feedback with swappedDown node.
+		if(thisLeft != null){
+			thisLeft.parent = parent;
+		}
+		if(thisRight != null){
+			thisRight.parent = parent;
+		}
 	}
 }
 
